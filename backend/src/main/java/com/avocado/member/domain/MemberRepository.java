af7780adfcb96d0a88b8638da004033de06cea39
juan-jung <crumbled23@gmail.com>
2023-07-24 09:41:10 +0900

    feat: login 구현

--- backend/src/main/java/com/avocado/member/domain/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.avocado.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
--- backend/src/main/java/com/avocado/member/domain/Status.java
+++
@@ -1,6 +0,0 @@
-package com.avocado.member.domain;
-
-// 일반 사용자, 관리자
-public enum Status {
-    BASIC, MANAGER
-}
