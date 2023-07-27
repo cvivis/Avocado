b33a101db88371011f4175ca1d917c43bd8bb528
cvivis <hsy0754@naver.com>
2023-07-27 14:01:52 +0900

    [Feat] : front 테스트 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import NormalBid from './normalBid';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      실행완료
+      <p>실행완료</p>
+      <NormalBid ></NormalBid>
+      <NormalBid ></NormalBid>
+      <NormalBid ></NormalBid>
     </div>
   );
 }
