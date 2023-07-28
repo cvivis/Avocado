4a439916b4b85399d269d4e45ae1c3b6ac188db2
이원희 <thehee0407@naver.com>
2023-07-28 17:43:11 +0900

    <Feat>: FE 초안 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,25 @@ function App() {
   return (
     <div className="App">
       실행완료
+      
+      <div className="nav-btn">
+        <button>
+          <Link to="/"> HOME </Link>
+        </button>
+        <button>
+          <Link to="/about"> ABOUT </Link>
+        </button>
+        <button>
+          <Link to="/products"> PRODUCTS </Link>
+        </button>
+      </div>
+
+      <Routes>
+  <Route path="/" element={<Home />}></Route>
+  <Route path="/about/*" element={<About />}></Route>
+  <Route path="/products" element={<Products />}></Route>
+</Routes>
+    
     </div>
   );
 }
