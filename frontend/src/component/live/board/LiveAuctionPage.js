c5fa9c64d29b71e449028ab897b4349ca6abd264
오승기 <dhtmdrl2004@knou.ac.kr>
2023-08-10 10:19:37 +0900

    방송화면 파트 develop에 merge

---
+++ frontend/src/component/live/board/LiveAuctionPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+
+} from "@chakra-ui/react";
+
+function LiveAuctionPage() {
+    
+
+    return (
+        <>
+            잘 나옵니까
+        </>
+    )
+}
+
+export default LiveAuctionPage;
