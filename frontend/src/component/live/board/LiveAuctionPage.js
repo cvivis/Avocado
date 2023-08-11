aa169ab7fef8e2dae6ed9d520d1c32893e831061
오승기 <dhtmdrl2004@knou.ac.kr>
2023-08-11 14:52:20 +0900

    <Feat> : 방송 편성표 화면 구성 및 DatePicker 구현 완료

--- frontend/src/component/live/board/LiveAuctionPage.js
+++ frontend/src/component/live/board/LiveAuctionPage.js
@@ -1,71 +1,72 @@
 import { React, useState, forwardRef } from "react";
 import {
-    Flex, Spacer, Box, Center, Card, Button,
+    Flex, Spacer, Box, Center, Card, Button, Text,
 } from "@chakra-ui/react";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import '../board/LiveAuction.css';
 import { ko } from "date-fns/esm/locale";
+import { getMonth, getDate } from "date-fns";
 
 
 function LiveAuctionPage() {
     
     const [startDate, setStartDate] = useState(new Date());
+    const [curBroadcast, setCurBroadcast] = useState(null);
     const now = new Date();
     const max = new Date(Date.parse(now)+30*1000*60*60*24); // 한달 리미트
-    const CustomInput = forwardRef(({ value, onClick }, ref) => (
-        <Button className="custom-input" onClick={onClick} ref={ref}    
-            bg={'green'}
-            color={'white'}
-            w={'400px'}
-            fontSize={'3xl'}
-            _hover={{ bg: "green" }}
-            mt={'10px'}
+
+    const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+        <Button
+            className="example-custom-input" onClick={onClick} ref={ref}
+            bg={'green'} _hover={{ bg: "green" }} color={'white'}
+            fontSize={'3xl'} w={'400px'}
         >
-            {value}
+        {value}
         </Button>
     ));
+
+    function ListBox() {
+
+        if(curBroadcast === null) {
+            return (
+                <>
+                    <Text as={'b'} fontSize={'xl'}>방송 날짜를 먼저 선택해 주세요</Text>
+                </>
+            )
+        }
+
+        return (
+            <></>            
+        )
+    }
 
     return (
         <>
             <Flex mt={'20px'}>
                 <Spacer />
-                <Card w={'40%'} h={'65vh'}>
+                <Card w={'40%'} h={'80vh'}>
                     <Center>
                         <DatePicker
                             calendarClassName="date-picker-calendar"
-                            renderCustomHeader={({
-                                date,
-                                changeYear,
-                                changeMonth,
-                                decreaseMonth,
-                                increaseMonth,
-                                prevMonthButtonDisabled,
-                                nextMonthButtonDisabled,
-                              }) => (
-                                <Flex bg={'green'}>
-                                    <Button onClick={decreaseMonth} disabled={prevMonthButtonDisabled} bg={'green'} _hover={{ bg: "green" }} color={'white'}>
-                                        {"<"}
-                                    </Button>
-                                    <Button onClick={increaseMonth} disabled={nextMonthButtonDisabled} bg={'green'} _hover={{ bg: "green" }} color={'white'}>
-                                        {">"}
-                                    </Button>
-                                </Flex>
-                            )}
+                            className="date-picker"
                             locale={ko}
-                            dateFormat='yyyy년 MM월 dd일' // 날짜 형태
+                            dateFormat='yyyy년 MM월 dd일 (eee)' // 날짜 형태
+                            dateFormatCalendar="yyyy년 MM월"
                             shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
                             minDate={new Date()} // minDate 이전 날짜 선택 불가
                             maxDate={max} // maxDate 이후 날짜 선택 불가
                             selected={startDate}
                             onChange={(date) => setStartDate(date)}
-                            customInput={<CustomInput />}
+                            customInput={<ExampleCustomInput />}
+                            showPopperArrow={false}
                         />
                     </Center>
                 </Card>
                 <Spacer />
-                <Card w={'40%'} h={'65vh'}>
+                <Card w={'40%'} h={'80vh'}>
                     <Center> 
-                        오른쪽 박스
+                        <ListBox />
                     </Center>
                 </Card>
                 <Spacer />
