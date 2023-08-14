650a58aae73053c1e6933a369d059f64c0ef1d79
황시은 <hsy0754@naver.com>
2023-08-14 10:58:28 +0900

    배포상황에서 api, websocket 연결 확인

--- backend/src/main/java/com/avocado/normal/board/domain/repository/NormalBoardRepository.java
+++ backend/src/main/java/com/avocado/normal/board/domain/repository/NormalBoardRepository.java
@@ -1,3 +1,4 @@
+//NormalBoardRepository
 package com.avocado.normal.board.domain.repository;
 
 import com.avocado.Item.domain.entity.Category;
@@ -16,17 +17,15 @@ public interface NormalBoardRepository extends JpaRepository<Item, Long> {
     // save는 추후에
 
     // 전체 리스트 반환
-    @Query("SELECT NEW com.avocado.normal.board.controller.dto.NormalResponseEntryDto(nh.normalAuction.id,i.id, i.name, i.content, nh.bidPrice, i.hopePrice, a.startAt, a.endAt) " +
+    @Query("SELECT NEW com.avocado.normal.board.controller.dto.NormalResponseEntryDto(a.id,i.id, i.name, i.content, i.hopePrice, a.startAt, a.endAt) " +
             "FROM item i " +
-            "JOIN normalAuction a ON i.id = a.item.id " +
-            "JOIN normalHistory nh ON a.id = nh.normalAuction.id")
+            "JOIN normalAuction a ON i.id = a.item.id ")
     List<NormalResponseEntryDto> findAllItemList();
 
     // 검색 리스트 반환
-    @Query("SELECT NEW com.avocado.normal.board.controller.dto.NormalResponseEntryDto(nh.normalAuction.id,i.id, i.name, i.content, nh.bidPrice, i.hopePrice, a.startAt, a.endAt) " +
+    @Query("SELECT NEW com.avocado.normal.board.controller.dto.NormalResponseEntryDto(a.id,i.id, i.name, i.content, i.hopePrice, a.startAt, a.endAt) " +
             "FROM item i " +
-            "JOIN normalAuction a ON i.id = a.item.id " +
-            "JOIN normalHistory nh ON a.id = nh.normalAuction.id " +
+            "JOIN normalAuction a ON i.id = a.item.id "+
             "WHERE i.name LIKE :keyword") // 직접적인 와일드카드 작성 불가
     List<NormalResponseEntryDto> findByItemNameContains(@Param("keyword") String keyword);
 
@@ -38,10 +37,9 @@ public interface NormalBoardRepository extends JpaRepository<Item, Long> {
     NormalItemDetailResponseDto findDetailById(@Param("id") Long id);
 
     // 카테고리 리스트 보기
-    @Query("SELECT NEW com.avocado.normal.board.controller.dto.NormalResponseEntryDto(nh.normalAuction.id,i.id, i.name, i.content, nh.bidPrice, i.hopePrice, a.startAt, a.endAt) " +
+    @Query("SELECT NEW com.avocado.normal.board.controller.dto.NormalResponseEntryDto(a.id,i.id, i.name, i.content, i.hopePrice, a.startAt, a.endAt) " +
             "FROM item i " +
             "JOIN normalAuction a ON i.id = a.item.id " +
-            "JOIN normalHistory nh ON a.id = nh.normalAuction.id " +
             "WHERE i.category = :category") // 직접적인 와일드카드 작성 불가
     List<NormalResponseEntryDto> findAllByCategoryEquals(@Param("category") Category category);
 }
