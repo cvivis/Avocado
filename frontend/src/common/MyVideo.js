aeb58d1df039c98e68c7ecd4e5a1b93adfe6a53e
이원희 <thehee0407@naver.com>
2023-08-16 16:13:54 +0900

    <Refact> : 리팩토링

--- frontend/src/common/MyVideo.js
+++ frontend/src/common/MyVideo.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import Openvidu from "../component/openvidu/OnlineMeeting"
 
-function MyVideo() {
-
+function MyVideo(props) {
+    console.log(props.useId+"마이비디오 유즈아이디이이이")
     return(
         // 사이즈 추후에 수정
         <Box> 
-            <Openvidu />
+            <Openvidu useId = {props.useId} />
         </Box>
     )
 }
