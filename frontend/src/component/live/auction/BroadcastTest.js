90d5c2e82163f55460509bd18955e1e6c98afb19
정재현 <crumbled23@gmail.com>
2023-08-17 16:14:21 +0900

    Update BroadcastTest.js

--- frontend/src/component/live/auction/BroadcastTest.js
+++ frontend/src/component/live/auction/BroadcastTest.js
@@ -111,8 +111,8 @@ const navigate = useNavigate();
     
     const connect = () =>{
     client.current = new StompJs.Client({
-        // brokerURL: 'wss://i9a407.p.ssafy.io:8080/ws/live-auction',
-        brokerURL: 'ws://localhost:8080/ws/live-auction',
+        brokerURL: 'wss://i9a407.p.ssafy.io:8080/ws/live-auction',
+        //brokerURL: 'ws://localhost:8080/ws/live-auction',
         onConnect:() =>{
             console.log('소켓 연결 성공')
             subcribe();
