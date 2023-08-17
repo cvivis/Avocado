d377f2325038977b0b529cb7480ff22a4bb35c77
juan-jung <crumbled23@gmail.com>
2023-08-17 15:18:25 +0900

    s3추가

---
+++ backend/src/main/java/com/avocado/test/TestService.java
@@ -0,0 +1,20 @@
+package com.avocado.test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+    private final S3Uploader s3Uploader;
+
+    @Transactional
+    public void create(String name, MultipartFile file) {
+        String url = "";
+        if(file != null)  url = s3Uploader.uploadFileToS3(file, "static/test");
+
+    }
+}
+
